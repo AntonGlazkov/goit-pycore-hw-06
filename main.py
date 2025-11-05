2356bc1982a5070d5168fb0389bc3844eb3c5de9
AntonGlazkov <a.glazkoff@gmail.com>
2025-11-05 09:41:44 +0000

    dz_9

---
+++ main.py
@@ -0,0 +1,62 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value: str):
+        if not (isinstance(value, str) and value.isdigit() and len(value) == 10):
+            raise ValueError("Phone number must contain exactly 10 digits.")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name: str):
+        self.name = Name(name)
+        self.phones: list[Phone] = []
+
+    def add_phone(self, phone: str) -> None:
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone: str) -> None:
+        for i, p in enumerate(self.phones):
+            if p.value == phone:
+                self.phones.pop(i)
+                break
+
+    def edit_phone(self, old_phone: str, new_phone: str) -> None:
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                break
+
+    def find_phone(self, phone: str) -> Phone | None:
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, record: Record) -> None:
+        self.data[record.name.value] = record
+
+    def find(self, name: str) -> Record | None:
+        return self.data.get(name)
+
+    def delete(self, name: str) -> None:
+        if name in self.data:
+            del self.data[name]
